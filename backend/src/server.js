3799ab59ccafc9459c068efb459da84f387040e2
Murilo Mota <murilohfmota@gmail.com>
2020-09-14 16:47:17 -0300

    return res.send alterado para return res.jason, a forma de resposta do servisor agora passa a ser pelo obejto messege

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const server = express();
 // GET, Post, req, res
 server.get('/', (req, res) =>{
- return res.send('hellou world');
+    
+ return res.json({massage : `hellou ${req.query.name}`});
 });
 server.listen(3333);
