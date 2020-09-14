3f54cad098b859760230d894206d6d389d7f7fa9
Murilo Mota <murilohfmota@gmail.com>
2020-09-14 15:50:38 -0300

    estabelecendo porta de servidor e requisição e respostar do server como "hellou wolrd"

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,8 @@
 const express = requestAnimationFrame('express');
 
 const server = express();
+// GET, Post, req, res
+server.get('/', (req, res) =>{
+ return res.send('hellou world');
+});
+server.listen(3333);
